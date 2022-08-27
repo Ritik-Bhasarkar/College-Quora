3930a61f037b1ca5dfe9a271d2434af955a8ba8b
Ritik Bhasarkar <ritikbhasarkar2401@gmail.com>
2022-08-27 16:45:58 +0530

    updated

---
+++ src/Sidebar.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Sidebar.css";
+import SidebarOption from "./SidebarOption";
+import FormatQuoteIcon from "@mui/icons-material/FormatQuote";
+import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
+import ExploreOutlinedIcon from "@mui/icons-material/ExploreOutlined";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+import MailOutlineRoundedIcon from "@mui/icons-material/MailOutlineRounded";
+import BookmarkBorderRoundedIcon from "@mui/icons-material/BookmarkBorderRounded";
+import ListAltRoundedIcon from "@mui/icons-material/ListAltRounded";
+import PermIdentityRoundedIcon from "@mui/icons-material/PermIdentityRounded";
+import MoreHorizRoundedIcon from "@mui/icons-material/MoreHorizRounded";
+import Button from "@mui/material/Button";
+
+function Sidebar() {
+  return (
+    <div className="sidebar ">
+      <FormatQuoteIcon />
+      <SidebarOption active Icon={HomeRoundedIcon} text="Home" />
+      <SidebarOption Icon={ExploreOutlinedIcon} text="Explore" />
+      <SidebarOption Icon={NotificationsNoneOutlinedIcon} text="Notification" />
+      <SidebarOption Icon={MailOutlineRoundedIcon} text="Messages" />
+      <SidebarOption Icon={BookmarkBorderRoundedIcon} text="Bookmark" />
+      <SidebarOption Icon={ListAltRoundedIcon} text="Lists" />
+      <SidebarOption Icon={PermIdentityRoundedIcon} text="profile" />
+      <SidebarOption Icon={MoreHorizRoundedIcon} text="More" />
+
+      <Button varinat="outlined" className="sidebar__ask" fullWidth>
+        Ask
+      </Button>
+    </div>
+  );
+}
+
+export default Sidebar;
