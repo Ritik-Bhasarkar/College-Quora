8305864a8eee008eced475e1b327721699d021b0
Ritik Bhasarkar <ritikbhasarkar2401@gmail.com>
2022-09-01 17:31:18 +0530

    Added QueBox and Started making Post

--- src/QueBox.js
+++ src/QueBox.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./QueBox.css";
 import { Avatar, Button } from "@mui/material";
+//import PhotoSizeSelectActualOutlinedIcon from "@mui/icons-material/PhotoSizeSelectActualOutlined";
 
 function QueBox() {
   return (
@@ -10,7 +11,19 @@ function QueBox() {
           <Avatar src="https://i.pinimg.com/736x/f3/43/68/f34368a931299ac106b7c1f6c3484627.jpg" />
           <input placeholder="What's your Query" type="text"></input>
         </div>
-        <Button>Post</Button>
+        {/* <div className="queBox__inputImageIcon">
+          <input
+            style={{ display: "none" }}
+            // accept="image/png, image/gif, image/jpeg"
+            accept="image/*"
+            id="queBox_inpur_Image"
+            type="file"
+          ></input>
+          <label htmlFor="choose-file">
+            <PhotoSizeSelectActualOutlinedIcon />
+          </label>
+        </div> */}
+        <Button className="queBox_postButton"><h3>post</h3></Button>
       </form>
     </div>
   );
